dc9cef9d1b05b5cec2e80b501ff6277975589481
Viktor Lin <asfgjk123123@gmail.com>
2015-04-07 12:05:09 +0800

    first commit

---
+++ pelicanconf.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Zekt'
+SITENAME = '源夢'
+SITESUBTITLE = '在CS與IE之間遊蕩卻好像啥都不會的小渣渣(・ω・)'
+SITEURL = ''#'https://Zekt.github.io/Blog'
+THEME = '/home/vik/Projects/Blog/themes/brownstone'
+
+PATH = ''
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = 'zh'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'))
+
+# Social widget
+SOCIAL = (('Facebook', 'https://www.facebook.com/victorreznovisalive'),
+        ('Github', 'https://github.com/Zekt'))
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
