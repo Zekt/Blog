0d0d4703adf8571a7843436883741abd48a64b38
Viktor Lin <asfgjk123123@gmail.com>
2015-12-07 06:07:01 +0800

    I don't know.

--- pelicanconf.py
+++ pelicanconf.py
@@ -32,6 +32,14 @@ SOCIAL = (('Facebook', 'https://www.facebook.com/victorreznovisalive'),
 
 DEFAULT_PAGINATION = 10
 
+# DISQUS
 DISQUS_SITENAME = 'zekt'
+
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+# Elegant
+LANDING_PAGE_ABOUT = {
+        'title': '在CS與IE中徘徊的弱弱大學生',
+        'details': '<p>你好，我是林子期，目前就讀臺大資管系一年級。興趣是寫程式跟看書，喜歡電腦科學、資訊工程，有點愚昧但喜歡思考，想努力的滿足自己的無知。</p><p>高中時打過一些演算法類競賽，略懂一些C/C++和演算法與資料結構，會一點點Python及網頁前後端，希望每天都能回答自己更多問題，期待能對社會上的不合理有更深層的省思，夢想為自己與他人找到目標。</p>'
+        }
