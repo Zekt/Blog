4ce0328cc969b855db1c10839bda32be1746c9a0
Viktor Lin <asfgjk123123@gmail.com>
2017-05-11 05:52:25 +0800

    modify deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,9 @@ if [ -n "$(git status --porcelain)" ]; then
 fi
 hugo --theme=hugo-theme-cactus-plus
 git checkout gh-pages
+git add public && git stash && rm -rf ./* && git stash pop && mv public/* ./ && rm -r public && git add -A
+git status
 echo -n "commit message:"
 read cms
-git add public && git stash && rm -rf ./* && git stash pop && mv public/* ./ && rm -r public && git add -A && git commit -m "$cms"
+git commit -m "$cms"
 echo "changes committed to gh-pages branch, push to deploy."
