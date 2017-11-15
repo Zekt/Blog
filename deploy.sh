be18052a0d6b60d3346421e401b009220677f4bb
Viktor Lin <asfgjk123123@gmail.com>
2017-11-15 22:15:30 +0800

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 if [ -n "$(git status --porcelain)" ]; then
 	echo "changes not commited." && exit;
 fi
+git submodule update
 hugo --theme=hugo-theme-cactus-plus
 git checkout gh-pages
 git add public && git stash && rm -rf ./* && git stash pop && mv public/* ./ && rm -r public && git add -A
